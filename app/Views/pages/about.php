53cf135705ee2df2dd41d5eabbadcef7d52827b1
Rifan Setiadi <>
2022-08-31 20:52:18 +0700

    praktikum1
    praktikum1

---
+++ app/Views/pages/about.php
@@ -0,0 +1,2 @@
+<h3>Ini halaman about</h3>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut voluptates eveniet alias sequi quis placeat officia eos facilis sit numquam consectetur esse, itaque dolore ipsam inventore dolor, atque ullam? Ab?</p>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,2 @@
+<h3>ini halaman home</h3>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quam fugiat minima ex doloribus nisi consequuntur ipsam nulla nostrum, repudiandae vel vitae cupiditate ut expedita sed corrupti et in est!</p>
